78b71ddf3e812baa68a0230a9ab09c90748b8629
mrana007 <rana.es.bc@gmail.com>
2023-12-06 22:05:42 +0600

    implement fetching fake data

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div>
-      <h1>Welcome to next level data fetching module</h1>
+      <h1 className='text-4xl text-center'>Welcome to next level data fetching module</h1>
     </div>
   );
 };
---
+++ src/app/posts/page.js
@@ -0,0 +1,26 @@
+const PostsPage = async () => {
+  const res = await fetch("http://localhost:5000/posts", {
+    cache: "force-cache"
+  });
+  const posts = await res.json();
+  // console.log(posts);
+  return (
+    <div className="w-full">
+      <h1 className="text-2xl">Total Posts: {posts.length}</h1>
+      {posts.map((post) => (
+        <div key={post.id} className="card bg-gray-100 shadow-xl my-5 w-[70%] mx-auto">
+          <div className="card-body">
+            <h2 className="card-title">{post.title}</h2>
+            <p>{post.description}</p>
+            <p>Likes: {post.likesCount}</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">See More</button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PostsPage;
