fbf9cbcd840046af5c4a1770c973a621e88210c6
mrana007 <rana.es.bc@gmail.com>
2023-12-07 00:14:39 +0600

    rendering ssg static site generation for details

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -1,16 +1,27 @@
 import Link from "next/link";
 
+export async function generateStaticParams() {
+  const res = await fetch("http://localhost:5000/posts");
+  const posts = await res.json();
+  const ids = posts.map((post) => {
+    return {
+      id: post.id + "",
+    };
+  });
+  //   console.log(ids);
+  return ids;
+}
+
 const DetailPage = async ({ params }) => {
-  // console.log(params.id);
+  //   console.log(params.id);
   const res = await fetch(`http://localhost:5000/posts/${params.id}`);
   const post = await res.json();
-//   console.log(post);
+  //   console.log(post);
   return (
     <div>
       <div
         key={post.id}
-        className="card bg-gray-100 shadow-xl my-5 w-[70%] mx-auto"
-      >
+        className="card bg-gray-100 shadow-xl my-5 w-[70%] mx-auto">
         <div className="card-body">
           <h2 className="card-title">{post.title}</h2>
           <p>{post.description}</p>
