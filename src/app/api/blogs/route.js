e4a2dceff6575302d050ca63ec2a263e4088009c
mrana007 <rana.es.bc@gmail.com>
2023-12-07 22:35:06 +0600

    implement basic idea on api routes

---
+++ src/app/api/blogs/route.js
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+// GET API
+export const GET = () => {
+  return NextResponse.json({ message: "Hello, World" });
+};
+
+// POST API
+export const POST = () => { };
+
+
+// PATCH API
+export const PATCH = () => { };
+
+
+// DELETE API
+export const DELETE = () => { };
